345068e392cc11da0c1a2c89fe1030f9533bde51
wjrekdgl <akdlgkem@maroo-on.com>
2023-01-10 18:01:28 +0900

    5.1 연관관계 매핑 종류_itementity 에러 수정

--- src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -2,14 +2,12 @@ package com.shop.config;
 
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import javax.naming.AuthenticationException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.rowset.serial.SerialException;
 import java.io.IOException;
 
-public class CustonAuthenticationEnttryPoint implements AuthenticationEntryPoint {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse response,
