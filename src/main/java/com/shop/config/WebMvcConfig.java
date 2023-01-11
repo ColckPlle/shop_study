f827ffd50e984e7028831d3c0ad42b63a80ff7a4
wjrekdgl <akdlgkem@maroo-on.com>
2023-01-11 17:28:13 +0900

    책 244페이지 까지 되돌림

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${uploadPaht}")
+    @Value("${uploadPath}")
     String uploadPath;
 
     @Override
