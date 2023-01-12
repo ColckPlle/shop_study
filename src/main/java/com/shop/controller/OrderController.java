3c38190358299acf53bbff919b815d83fed56792
wjrekdgl <akdlgkem@maroo-on.com>
2023-01-12 16:23:41 +0900

    7.1 주문기능 구현 305

--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.shop.controller;
 
 import com.shop.dto.OrderDto;
-import com.shop.dto.OrderHistDto;
 import com.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -50,16 +49,5 @@ public class OrderController {
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
 
-    @GetMapping(value = {"/orders", "/orders/{page}"})
-    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model){
-        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
-
-        Page<OrderHistDto> ordersHistDtoList = orderService.getOrderList(principal.getName(), pageable);
-
-        model.addAttribute("orders", ordersHistDtoList);
-        model.addAttribute("page", pageable.getPageNumber());
-        model.addAttribute("maxPage", 5);
-        return "order/orderHist";
-    }
 
 }
