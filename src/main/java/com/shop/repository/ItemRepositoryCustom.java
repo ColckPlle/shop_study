917c68f1753cd2d521a2ee6e19465ecc7e83233d
wjrekdgl <akdlgkem@maroo-on.com>
2023-01-12 13:45:59 +0900

    6.3 상품 관리하기 완 - 279

--- src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -1,7 +1,6 @@
 package com.shop.repository;
 
 import com.shop.dto.ItemSearchDto;
-import com.shop.dto.MainItemDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +8,4 @@ import org.springframework.data.domain.Pageable;
 public interface ItemRepositoryCustom {
 
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
-    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
-
-
 }
