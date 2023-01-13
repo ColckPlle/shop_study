66467572bb4118bd12b5e01f2996f1557c1e4ae1
wjrekdgl <akdlgkem@maroo-on.com>
2023-01-13 11:26:50 +0900

    7.1 -308까지의 에러 수정

--- src/main/java/com/shop/dto/ItemImgDto.java
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -14,6 +14,10 @@ public class ItemImgDto {
 
     private String oriImgName;
 
+    private String imgUrl;
+
+    private String repImgYn;
+
     private static ModelMapper modelMapper = new ModelMapper();
 
     public static ItemImgDto of(ItemImg itemImg){
